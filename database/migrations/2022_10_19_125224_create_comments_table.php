a896f8e317a930c34672ff557d7710cbc7de6a6c
eliyajoseph7 <exaveryeliya20@gmail.com>
2023-02-08 09:33:04 +0300

    image column added

--- database/migrations/2022_10_19_125224_create_comments_table.php
+++ database/migrations/2022_10_19_125224_create_comments_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
             $table->string('comment');
+            $table->string('image')->nullable();
             $table->timestamps();
         });
     }
