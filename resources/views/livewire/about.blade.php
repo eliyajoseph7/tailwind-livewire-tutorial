98d394fa7941c80995c610743f4845f4ce70c1b0
eliyajoseph7 <exaveryeliya20@gmail.com>
2023-02-22 16:42:35 +0300

    modified

---
+++ resources/views/livewire/about.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+@section('content')
+    <div class="container bg-white min-h-screen m-auto">
+        <div class="p-10">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Distinctio illo, dicta, vel quod laudantium sapiente odit dolorum consectetur ipsam sint, eveniet accusantium ratione harum cupiditate repellat molestias assumenda ea recusandae?
+        </div>
+
+    </div>
+@endsection
